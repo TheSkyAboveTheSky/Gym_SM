104794c8195a37ab71c340e112300b69695af093
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2022-06-25 05:02:56 +0000

    install react router and ui/material + pages+ components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>App</div>
+    <Box width="400px">
+      <Navbar />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/exercise/:id" element={<ExerciseDetail/>} /> 
+      </Routes>
+      </BrowserRouter>
+      <Footer />
+    </Box>
   )
 }
 
